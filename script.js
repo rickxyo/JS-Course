4862ea4cd9e0f51fc53e0f953b4395d9a73b2275
Pedro H. Oldoni <144467616+rickxyo@users.noreply.github.com>
2024-03-17 20:51:01 -0300

    operator precedence

--- script.js
+++ script.js
@@ -28,27 +28,35 @@
 
 // basic operators
 
-// Math Operators
+// // Math Operators
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+// const firstName = "Pedro Henrique";
+// const lastName = "Oldoni";
+// console.log(firstName + " " + lastName);
+
+// // Assignment operators
+// let x = 10 + 5; //15
+// x += 10; // x = x + 10 = 25
+// x *= 4; //x = x * 4 = 100
+// x++; //x = x + 1
+// console.log(x);
+
+// // Comparison operators
+// console.log(ageJonas > ageSarah);
+// console.log(ageSarah >= 18);
+
+// console.log(now - 1991 > now - 2018);
+
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
-
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
-
-const firstName = "Pedro Henrique";
-const lastName = "Oldoni";
-console.log(firstName + " " + lastName);
-
-// Assignment operators
-let x = 10 + 5; //15
-x += 10; // x = x + 10 = 25
-x *= 4; //x = x * 4 = 100
-x++; //x = x + 1
-console.log(x);
-
-// Comparison operators
-console.log(ageJonas > ageSarah);
-console.log(ageSarah >= 18);
-
 console.log(now - 1991 > now - 2018);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
