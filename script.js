3d62be687b6e1afef973074fc75432f370193d95
Pedro H. Oldoni <144467616+rickxyo@users.noreply.github.com>
2024-03-17 20:39:29 -0300

    basic operators

--- script.js
+++ script.js
@@ -14,14 +14,41 @@
 // javascriptIsFun = "YES!";
 // console.log(javascriptIsFun);
 
-// old way, do not use
-var age = 20;
-age = 21;
+// // old way, do not use
+// var agePedro = 20;
+// agePedro = 21;
 
-// let can change during the code
-let age = 30;
-age = 31;
+// // let can change during the code
+// let age = 30;
+// age = 31;
 
-// const cannot be change
-const birthYear = 1991;
-birthYear = 1990;
+// // const cannot be change
+// const birthYear = 1991;
+// birthYear = 1990;
+
+// basic operators
+
+// Math Operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = "Pedro Henrique";
+const lastName = "Oldoni";
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5; //15
+x += 10; // x = x + 10 = 25
+x *= 4; //x = x * 4 = 100
+x++; //x = x + 1
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+console.log(now - 1991 > now - 2018);
