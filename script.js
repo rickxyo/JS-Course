78066333c5a6daaa73466ff8ff5e1526f805b947
Pedro H. Oldoni <144467616+rickxyo@users.noreply.github.com>
2024-03-17 20:06:20 -0300

    var let const

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-console.log("Pedro");
+// console.log("Pedro");
 
-//Variable
-let firstName = "Pedro";
-console.log(firstName);
+// //Variable
+// let firstName = "Pedro";
+// console.log(firstName);
 
-// boolean
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+// // boolean
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
 
-javascriptIsFun = "YES!";
-console.log(javascriptIsFun);
+// javascriptIsFun = "YES!";
+// console.log(javascriptIsFun);
+
+// old way, do not use
+var age = 20;
+age = 21;
+
+// let can change during the code
+let age = 30;
+age = 31;
+
+// const cannot be change
+const birthYear = 1991;
+birthYear = 1990;
