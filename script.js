529c4e26288440db2a8ff748da44fe1d775bec44
Pedro H. Oldoni <144467616+rickxyo@users.noreply.github.com>
2024-03-17 08:56:57 -0300

    boolean

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ console.log("Pedro");
 //Variable
 let firstName = "Pedro";
 console.log(firstName);
+
+// boolean
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
